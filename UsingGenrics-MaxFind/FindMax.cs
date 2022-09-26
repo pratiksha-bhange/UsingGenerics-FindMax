c2f35849fc703b4100c63752326c5f566233b459
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-26 23:51:04 +0530

    [pratuu]ADDED: Find Max Number among three intergers

---
+++ UsingGenrics-MaxFind/FindMax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingGenrics_MaxFind
+{
+   public class FindMax
+    {
+        public int findMaxIntValue(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else
+            {
+                return thirdValue;
+            }
+        }
+    }
+}
--- UsingGenrics-MaxFind/Program.cs
+++ UsingGenrics-MaxFind/Program.cs
@@ -7,6 +7,8 @@ namespace UsingGenrics_MaxFind
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find Maximum Using Generics");
+            FindMax findMaximum = new FindMax();
+            Console.WriteLine("Maximum Number is: " + findMaximum.findMaxIntValue(45, 89, 67));
         }
     }
 }
