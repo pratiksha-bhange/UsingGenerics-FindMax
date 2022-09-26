589a92915edcb30648106d900bba9e5a082504d5
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-26 23:35:12 +0530

    [pratuu]ADDED: Welcome to find max

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsingGenrics_MaxFind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Maximum Using Generics");
+        }
+    }
+}
